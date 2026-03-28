c642db6707f7a5a2e230b292a33e5fd80c53ffa1
Andrés <andres.flowers306@gmail.com>
2026-03-28 18:22:05 +0100

    tic tac toe online v2

--- app/tic-tac-toe/online/[gameId]/page.tsx
+++ app/tic-tac-toe/online/[gameId]/page.tsx
@@ -5,19 +5,20 @@ import { getTicTacToeOnlineGameSummary } from "@/lib/services/tictactoe.service"
 import { TicTacToeOnlineGame } from "@/components/tictactoe/tictactoe-online-game"
 
 type PageProps = {
-  params: {
+  params: Promise<{
     gameId: string
-  }
+  }>
 }
 
 export default async function TicTacToeOnlineGamePage({ params }: PageProps) {
+  const { gameId } = await params
   const session = await getServerSession(authOptions)
   if (!session?.user?.discordId) {
     redirect("/api/auth/signin/discord?callbackUrl=/tic-tac-toe")
   }
 
   await syncDiscordUser(session.user.discordId, session.user.image ?? null)
-  const game = await getTicTacToeOnlineGameSummary(params.gameId, session.user.discordId)
+  const game = await getTicTacToeOnlineGameSummary(gameId, session.user.discordId)
 
   if (!game) {
     redirect("/tic-tac-toe")
